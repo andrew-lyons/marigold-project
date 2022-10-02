76e77b2858f8b515bc5e034a7aa554c6065d2790
Andrew Lyons <aslyons001@gmail.com>
2022-10-02 13:01:50 -0400

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ git init
 git add -A
 git commit -m 'deploying...'
 
-git push -f git@github.com:andrew-lyons/magnolia-project.git main:gh-pages
+git push -f git@github.com:andrew-lyons/marigold-project.git main:gh-pages
 
 cd -
